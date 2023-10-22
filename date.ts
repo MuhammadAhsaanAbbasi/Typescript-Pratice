90fdac33a662a3fc3e3176f7e3e556a75b67a2e3
Muhammad Ahsaan Abbasi <mahsaanabbasi@gmail.com>
2023-10-22 08:46:31 +0500

    Add Files

---
+++ date.ts
@@ -0,0 +1,9 @@
+const futureDate = new Date();
+futureDate.setDate(10); // Set the day to the 10th.
+futureDate.setMonth(6); // Set the month to July (0-based).
+futureDate.setFullYear(2024); // Set the year to 2024.
+futureDate.setHours(3); // Set the hour to 3 PM.
+futureDate.setMinutes(30); // Set the minutes to 30.
+futureDate.setSeconds(0); // Set the seconds to 0.
+
+console.log(futureDate);
